5c06dcf75a21641a1e19900c8fa34cc9c334c35a
Filip Kujavec <kujavec.filip@gmail.com>
2021-07-12 15:43:57 +0200

    remove shadow from recipe img upload button

--- client/src/Pages/EditRecipe/Image.js
+++ client/src/Pages/EditRecipe/Image.js
@@ -26,7 +26,7 @@ export const RecipeImage = (props) => {
     return (
         <>
         <img className='object-cover h-full w-full' src={imageUrl} />
-        <svg onClick={() => setShowUploadModal(true)} className='absolute bottom-2 right-2 w-10 fill-current text-green-500 cursor-pointer shadow-lg transform hover:scale-125' xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" ><path d="M11 11h4a1 1 0 0 0 0-2h-4V5a1 1 0 0 0-2 0v4H5a1 1 0 1 0 0 2h4v4a1 1 0 0 0 2 0v-4zm-1 9C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10z"></path></svg>
+        <svg onClick={() => setShowUploadModal(true)} className='absolute bottom-2 right-2 w-10 fill-current text-green-500 cursor-pointer transform hover:scale-125' xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" ><path d="M11 11h4a1 1 0 0 0 0-2h-4V5a1 1 0 0 0-2 0v4H5a1 1 0 1 0 0 2h4v4a1 1 0 0 0 2 0v-4zm-1 9C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10z"></path></svg>
         {showUploadModal ? <UploadModal onEscape={onEscape} handleFile={handleFile} fileName={id} label='Upload Recipe Image' acceptedFiles='*.png *.jpeg' />:null}
         </>
     );
